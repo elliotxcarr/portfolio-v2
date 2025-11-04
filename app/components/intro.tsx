a7a090386421943a8b77c31fd5477e9446da6427
Elliot Carr <Elliot.Carr@navtor.com>
2025-11-04 14:38:04 +0000

    navbar and home page

---
+++ app/components/intro.tsx
@@ -0,0 +1,15 @@
+export default function Intro() {
+  return (
+    <div className='flex flex-row w-full h-130 px-15'>
+      <div className='flex flex-col justify-center '>
+          <div className='text-6xl flex flex-col text-white font-semibold gap-2'>Elliot Carr
+            <div className=' h-1 bg-(--elliot-red)'></div>
+          </div>
+          <div className='mt-10 flex flex-col gap-3'>
+            <h1 className='text-2xl '>Junior Software Developer</h1>
+            <h1 className='text-2xl'>First Class BSc Hons in Digital Media</h1>
+          </div>
+      </div>
+    </div>
+  )
+}
---
+++ app/components/navbar.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import './navbar.css';
+
+export default function Navbar() {
+  return (
+    <div className='w-full top-0 h-14 px-20'>
+      <div className='flex items-center h-full  flex-row justify-between'>
+        <Image
+          className=""
+          src="/Logo.png"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
+          <ul className='w-1/3 flex flex-row justify-around'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+      </div>
+    </div>
+  )
+}
